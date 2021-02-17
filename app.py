b3b2bbe77418a4db833d6807d598cd3ac8b09b43
Charly Wargnier <cwar05@gmail.com>
2021-02-17 10:58:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 
 #import networkx as nx
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import matplotlib.pyplot as plt
+#import seaborn as sns
 
 from bs4 import BeautifulSoup
 #import pywikibot
